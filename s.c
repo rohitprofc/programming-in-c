065afb893550ccbead4f0994b050c418f4d26b8c
rohitprofc <rohitprofc@gmail.com>
2022-03-30 21:11:06 +0530

    Mar 30

--- s.c
+++ s.c
@@ -3,14 +3,10 @@ int gcd(int a,int b)
 {
     if(a==0)
     return b;
-    if(b==0)
+    else if(b==0)
     return a;
-    if (a==b)
-    return a;
-    if(a>b)
-    return gcd(a-b,b);
-    else
-    return gcd(a,b-a);
+    else 
+    return gcd(b,a%b);
 }
 int main()
 {
