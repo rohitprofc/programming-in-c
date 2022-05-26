afd19ab9ac0df8d839bfb06438dbdfb2c6c9ad02
Rohit <100615505+rohitprofc@users.noreply.github.com>
2022-05-26 16:37:11 +0530

    May 26

--- Lab Programs/a.c
+++ Lab Programs/a.c
@@ -11,5 +11,3 @@ I'm Rohit
 I play chess
 what about a game??
 */
-
-![jweDN93wtbUgg2eO8eYTWV6QriS-the-queens-gambit-43678355-1280-720](https://user-images.githubusercontent.com/100615505/170476082-86a1f446-4f0e-467c-8f77-a3358a4f4c58.jpg)
